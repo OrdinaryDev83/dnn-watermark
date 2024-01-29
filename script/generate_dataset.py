dfee82c610f89b766c89478196316b21df95c252
Theo Ripoll <theo.ripoll@epita.fr>
2024-01-29 11:45:19 +0100

    new method to remove background from logo

--- script/generate_dataset.py
+++ script/generate_dataset.py
@@ -4,6 +4,7 @@ import random
 import shutil
 import string
 from typing import List
+import cv2
 
 import matplotlib.font_manager as fm
 import matplotlib.patches as patches
@@ -193,8 +194,31 @@ for filename in os.listdir(os.path.join("data", "logos")):
 
 
 def remove_background(img: Image.Image) -> Image.Image:
-    img = img.convert("RGBA")
-    return img
+    img = np.array(img)
+    if len(img.shape) == 3:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    else: # if logo is already grayscale
+        gray = img
+    # threshold input image as mask
+    mask = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
+    # negate mask
+    mask = 255 - mask
+    # apply morphology to remove isolated extraneous noise
+    # use borderconstant of black since foreground touches the edges
+    kernel = np.ones((3,3), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # anti-alias the mask -- blur then stretch
+    # blur alpha channel
+    mask = cv2.GaussianBlur(mask, (0,0), sigmaX=2, sigmaY=2, borderType = cv2.BORDER_DEFAULT)
+    # linear stretch so that 127.5 goes to 0, but 255 stays 255
+    mask = (2*(mask.astype(np.float32))-255.0).clip(0,255).astype(np.uint8)
+    # put mask into alpha channel
+    result = img.copy()
+    result = cv2.cvtColor(result, cv2.COLOR_BGR2BGRA)
+    result[:, :, 3] = mask
+    return Image.fromarray(result, 'RGBA')
+    
 
 
 print("Removing backgrounds from logos...")
