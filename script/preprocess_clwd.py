fa610af719a49e5000f58a6098a3d0d73ca4c9ef
quentin.fisch <quentin.fisch@epita.fr>
2024-02-02 17:20:50 +0100

    feat(clwd): fix clwd dataset generation

--- script/preprocess_clwd.py
+++ script/preprocess_clwd.py
@@ -9,6 +9,7 @@ from torch.utils.data import Dataset
 from torchvision.io import read_image
 from torchvision.ops import box_area, box_convert, masks_to_boxes
 from torchvision.transforms import functional as F
+from tqdm import tqdm
 
 
 class CLWD(Dataset):
@@ -27,7 +28,10 @@ class CLWD(Dataset):
 
         mask_path = os.path.join(self.masks_dir, f"{index}.png")
         mask = read_image(mask_path)
-        bbox = masks_to_boxes(mask)
+        try:
+            bbox = masks_to_boxes(mask)
+        except RuntimeError:
+            bbox = None
 
         target = 0  # logo class
 
@@ -50,10 +54,12 @@ def dataset_to_YOLO(dataset: CLWD) -> None:
         os.makedirs(dir, exist_ok=True)
 
     # convert the dataset to YOLO format
-    for i in range(1, len(dataset) + 1):
+    for i in tqdm(range(1, len(dataset) + 1)):
         image, bbox, target = dataset[i]
+        if bbox is None:
+            continue
         yolo_bbox = box_convert(bbox, in_fmt="xyxy", out_fmt="cxcywh")
-        yolo_bbox = yolo_bbox / image.size(0)
+        yolo_bbox = yolo_bbox / image.size(1)
 
         yolo_bbox = yolo_bbox.unique(dim=0)
 
@@ -61,7 +67,7 @@ def dataset_to_YOLO(dataset: CLWD) -> None:
         image.save(f"CLWD_dataset/train-yolo/images/{i}.jpg")
 
         with open(f"CLWD_dataset/train-yolo/labels/{i}.txt", "w") as f:
-            f.write(f"{target} {' '.join(map(str, bbox[0]))}\n")
+            f.write(f"{target} {' '.join(map(str, yolo_bbox[0].tolist()))}\n")
 
 
 def dataset_to_hf(dataset: CLWD) -> None:
@@ -75,8 +81,10 @@ def dataset_to_hf(dataset: CLWD) -> None:
     # create the directory
     os.makedirs("CLWD_dataset/train/images", exist_ok=True)
 
-    for i in range(1, len(dataset) + 1):
+    for i in tqdm(range(1, len(dataset) + 1)):
         image, bbox, target = dataset[i]
+        if bbox is None:
+            continue
         # save the image
         image = F.to_pil_image(image)
         image.save(f"CLWD_dataset/train/images/{i}.jpg")
@@ -89,9 +97,14 @@ def dataset_to_hf(dataset: CLWD) -> None:
             "bbox": bbox.tolist(),
             "id": i,
             "area": area,
-            "category_id": 0,
+            "category_id": target,
         }
 
         with open("CLWD_dataset/train/metadata.json", "a") as f:
             f.write(json.dumps(metadata))
             f.write("\n")
+
+
+if __name__ == "__main__":
+    dataset = CLWD("/Users/quentinfisch/Downloads/CLWD/train/images", "/Users/quentinfisch/Downloads/CLWD/train/masks")
+    dataset_to_YOLO(dataset)
